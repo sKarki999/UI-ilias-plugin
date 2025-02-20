d3a550e0303e6922fd2ef7155c736dcbe5dbd104
sKarki999 <sagun.karki27@gmail.com>
2025-02-20 23:13:34 +0100

    Implement UIHookGUI class for Plugin

---
+++ classes/class.ilCustomURLDisplayUIHookGUI.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Class ilCustomURLDisplayUIHookGUI
+ * 
+ */
+class ilCustomURLDisplayUIHookGUI extends ilUIHookPluginGUI {
+
+    public function __construct() {
+
+    }
+
+    // TODO:: To inject custom URL into UI
+    public function getHTML($a_comp, $a_part, $a_par = []): array {
+
+        return [];
+    }
+}
