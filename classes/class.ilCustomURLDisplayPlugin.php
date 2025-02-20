4f9002216729bfaf8e3c484ab3510eb5123fba4f
sKarki999 <sagun.karki27@gmail.com>
2025-02-20 19:34:21 +0100

    Add main Plugin class

---
+++ classes/class.ilCustomURLDisplayPlugin.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * URL Display User Interface Hook Plugin
+ *  
+ */
+class ilCustomURLDisplayPlugin extends ilUserInterfaceHookPlugin {
+    
+    /**
+     * @return string The name of the plugin
+     */
+    public function getPluginName(): string {
+        return "CustomURLDisplay";
+    }
+
+}
