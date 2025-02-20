f7b37d464a63d33b5e3f4af6675cffa8be8694d2
sKarki999 <sagun.karki27@gmail.com>
2025-02-20 18:50:30 +0100

    Initial Commit: Setting Plugin Metadata

---
+++ plugin.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+$id = "customurldisplay"; // Plugin ID
+$version = "1.0.0"; // Plugin version
+$ilias_min_version = "8.0"; // Minimum ILIAS Version
+$ilias_max_version = "8.999"; // Maximum ILIAS Version
+$responsible = "Sagun Karki";
+$responsible_mail = "sagun.karki27@gmail.com";
+?>
